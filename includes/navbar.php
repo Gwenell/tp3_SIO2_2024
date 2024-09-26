5d4983c0fd258362d99305d93f4558667514ee97
Gwenell <01gwenell@proton.me>
2024-09-26 11:39:39 +0200

    commit13

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,13 +1,13 @@
 <nav class="bg-gray-800 p-4">
     <div class="container mx-auto flex justify-between items-center">
         <div class="flex space-x-8">
-            <a href="saisirFicheFrais.php" 
-               class="<?php echo basename($_SERVER['PHP_SELF']) === 'saisirFicheFrais.php' ? 'text-gray-400 cursor-not-allowed' : 'text-white hover:text-gray-400'; ?> text-base">
-                Saisie Fiche de Frais
-            </a>
             <a href="liste_visiteur.php" 
                class="<?php echo basename($_SERVER['PHP_SELF']) === 'liste_visiteur.php' ? 'text-gray-400 cursor-not-allowed' : 'text-white hover:text-gray-400'; ?> text-base">
                 Liste des Visiteurs
+            </a>
+            <a href="saisirFicheFrais.php" 
+               class="<?php echo basename($_SERVER['PHP_SELF']) === 'saisirFicheFrais.php' ? 'text-gray-400 cursor-not-allowed' : 'text-white hover:text-gray-400'; ?> text-base">
+                Saisie Fiche de Frais
             </a>
         </div>
         
